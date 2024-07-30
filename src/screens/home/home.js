d708b3576bed1e2753968af108e5059ba29b06d0
RomeroArias23 <137248494+RomeroArias23@users.noreply.github.com>
2024-07-30 11:41:20 -0600

    home

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className='text'>
       <h1>{'{ YELLOW } Project'}</h1>
-      <p>El proyecto <strong>{ YELLOW }</strong> se inspira en el proyecto en inglés <strong>The Unsent Project</strong>, esta aplicación tiene como objetivo recopilar mensajes de texto no enviados a primeros amores, permitiendo a quienes participen expresar de manera anónima mensajes que no pudieron enviar en su momento.</p>
+      <p>El proyecto <strong>{ YELLOW }</strong> se inspira en el proyecto en inglés <strong><a href='https://theunsentproject.com/about'>The Unsent Project</a></strong>, esta aplicación tiene como objetivo recopilar mensajes de texto no enviados a primeros amores, permitiendo a quienes participen expresar de manera anónima mensajes que no pudieron enviar en su momento.</p>
 
       <p>El término "primer amor" se deja abierto a la interpretación, permitiendo que los mensajes se dirijan a amantes, mejores amigos, ex parejas, padres o madres, hermanxs, e incluso mascotas. Todos los textos enviados son almacenados en un archivo en línea y son dedicados a un remitente. Si quieres saber si alguien escribió una carta con tu nombre, puedes buscarlo.</p>
 
