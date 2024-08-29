0f6efddea8bdeaea7a3026d69ff24b79e20f5178
RomeroArias23 <137248494+RomeroArias23@users.noreply.github.com>
2024-08-29 14:44:09 -0600

    instructions

--- src/screens/lettersPage/lettersPage.js
+++ src/screens/lettersPage/lettersPage.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import LetterCard from '../../components/letters/letterCard';
+import './lettersPage.css';
 
 const LettersPage = () => {
   const [letters, setLetters] = useState([]);
 
   return (
-    <div>
+    <div className='letters-page'>
+      <p className='instructions'>Selecciona tu carta para que la puedas descargar</p>
       <LetterCard setLetters={letters}/>
     </div>
   )
